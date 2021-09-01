6b07d4d75cb5c0d4672d42d4c0aa4e2efbdf6901
forlearn1989 <forlearn1989@gmail.com>
2021-09-01 08:41:39 +0700

    Create new AWS  application

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload....Test CI when AWS off
+          Edit <code>src/App.js</code> and save to reload....Test CI when create new AWS
         </p>
         <a
           className="App-link"
